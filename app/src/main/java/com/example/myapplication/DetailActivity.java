2ea2d700a1f0d13a4838e183fbfab2017aa6f10e
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-02-25 03:18:47 -0600

    Used YouTubeAndroid Player API to play video in the Detail activity screen

--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -3,18 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.myapplication.models.Movies;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import org.parceler.Parcels;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends YouTubeBaseActivity {
 
+    public static final String YOUTUBE_API_KEY = "AIzaSyAEB-Ss-fu3_bqs0N7EA7ulXnP3oRZ4ewQ";
     TextView tvTitle;
     TextView tvOverview;
     RatingBar ratingBar;
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,7 @@ public class DetailActivity extends AppCompatActivity {
         tvTitle = findViewById(R.id.tvTitle);
         tvOverview = findViewById(R.id.tvOverview);
         ratingBar = findViewById(R.id.ratingBar);
+        youTubePlayerView = findViewById(R.id.player);
 
         //Retrieve the data in the detail activity screen corresponding to the key
         //Display the title in the activity details xml files so we can display in the screen the data we are pulling out
@@ -35,5 +43,21 @@ public class DetailActivity extends AppCompatActivity {
         //Convert Double to float, because Double has longer precision than float
         ratingBar.setRating((float) movie.getVote_rating());
 
+        //Display Video - We send request to YouTube API to play YouTube videos in android app
+        // Add YouTube jar file - Creating reference to YouTube library in this Android studio project
+        youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("DetailActivity", "onInitializationSuccess");
+                youTubePlayer.cueVideo("5xVh-7ywKpE");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("DetailActivity", "onInitializationFailure");
+            }
+        });
+
     }
 }
+
